20d778087d4bb6e07af554317ae3448666659c85
Thomas <thombsaway@gmail.com>
2022-02-21 13:17:41 +1100

    Add props for auth response

--- Weightlifting/API/DTOs/UserAuthenticationResponseDTO.cs
+++ Weightlifting/API/DTOs/UserAuthenticationResponseDTO.cs
@@ -2,5 +2,8 @@
 {
     public class UserAuthenticationResponseDTO
     {
+        public bool IsSuccess { get; set; }
+        public Dictionary<string, string>? Errors { get; set; }
+        public string? Token { get; set; }
     }
 }
