982f2960ee8cc189d5b52819053d47da581df79f
Thomas <thombsaway@gmail.com>
2022-02-27 18:58:12 +1100

    Register JWT and set up auth options.

--- Weightlifting/API/Program.cs
+++ Weightlifting/API/Program.cs
@@ -1,8 +1,12 @@
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.EntityFrameworkCore;
+
+using System.Text;
 
 using API.Data;
 using API.Data.Managers;
+using API.JWT;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +15,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 // Add authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWTSettings:Audience"],
 
+        ValidIssuer = builder.Configuration["JWTSettings:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:Key"]))
+    };
+});
 
 // Add JWTHandler rego
-
+builder.Services.AddScoped<JWTHandler>();
 
 // Configure Swagger to have auth option
 builder.Services.AddSwaggerGen();
