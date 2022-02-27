b6b537ce460b7530299313c3fe2e14c0954a158c
Thomas <thombsaway@gmail.com>
2022-02-27 18:43:37 +1100

    Stub datacontext

---
+++ Weightlifting/API/Data/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+using API.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DatabaseContext: IdentityDbContext<ApplicationUser>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
