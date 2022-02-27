13daf23bd5577a8dc2d6d495ee0c35a782509546
Thomas <thombsaway@gmail.com>
2022-02-27 16:13:53 +1100

    Added mapping profile.

---
+++ Weightlifting/API/Mapping/UserMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using API.Data.Models;
+using API.DTOs.Account;
+
+namespace API.Mapping
+{
+    public class UserMappingProfile: Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<UserRegistrationDTO, ApplicationUser>()
+                .ForMember(u => u.UserName, opt => opt.MapFrom(x => x.Email));
+        }
+    }
+}
