018c4a1a50a18f23b16d1cd69fc95d7be404c371
Thomas <thombsaway@gmail.com>
2022-02-28 16:03:06 +1100

    Added AddAthlete and GetAthletes endpoints

--- Weightlifting/API/Controllers/CoachesController.cs
+++ Weightlifting/API/Controllers/CoachesController.cs
@@ -2,17 +2,69 @@
 using Microsoft.AspNetCore.Authorization;
 
 using API.Data.Models;
+using API.DTOs.Coaches;
+using API.Data.Managers;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     public class CoachesController : Controller
     {
+        private readonly ICoachesManager _coachesManager;
+
+        public CoachesController(ICoachesManager coachesManager)
+        {
+            _coachesManager = coachesManager;
+        }
+
         [HttpGet("check")]
         [Authorize(AuthenticationSchemes = "Bearer", Roles = UserRoles.Coach)]
         public IActionResult Check()
         {
             return Ok("You're a coach!");
+        }
+
+        [HttpPost("athletes/add")]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = UserRoles.Coach)]
+        public async Task<IActionResult> AddAthlete(int athleteId)
+        {
+            var coachId = User.Identity.Name;
+
+            if (coachId is null)
+            {
+                return BadRequest("Error accessing identity");
+            }
+
+            var result = await _coachesManager.AddAthleteToCoach(coachId, athleteId);
+
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("athletes/get")]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = UserRoles.Coach)]
+        public async Task<IActionResult> GetAthletes()
+        {
+            var id = User.Identity.Name;
+
+            if (id is null) 
+            {
+                return BadRequest("Error accessing identity");
+            }
+
+            var result = await _coachesManager.GetAthletes(id);
+
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
         }
     }
 }
