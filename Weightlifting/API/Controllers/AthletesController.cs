d9308732f2f4f0866a86753b5a99418e4863e819
Thomas <thombsaway@gmail.com>
2022-02-28 12:04:01 +1100

    Add checking endpoints for athlete and coach.

---
+++ Weightlifting/API/Controllers/AthletesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+using API.Data.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AthletesController : Controller
+    {
+        [HttpGet("check")]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = UserRoles.Athlete)]
+        public IActionResult Check()
+        {
+            return Ok("You're an athlete!");
+        }
+    }
+}
---
+++ Weightlifting/API/Controllers/CoachesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+using API.Data.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CoachesController : Controller
+    {
+        [HttpGet("check")]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = UserRoles.Coach)]
+        public IActionResult Check()
+        {
+            return Ok("You're a coach!");
+        }
+    }
+}
