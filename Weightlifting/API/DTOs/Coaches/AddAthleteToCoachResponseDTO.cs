379577ad2e47a64a09f6cdb49a0b3315a94625e1
Thomas <thombsaway@gmail.com>
2022-02-28 16:04:20 +1100

    Add response DTOs for the new coach manager functions

---
+++ Weightlifting/API/DTOs/Coaches/AddAthleteToCoachResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs.Coaches
+{
+    public class AddAthleteToCoachResponseDTO
+    {
+        public bool Success { get; set; }
+        public Dictionary<string, string>? Errors { get; set; }
+    }
+}
---
+++ Weightlifting/API/DTOs/Coaches/GetAthletesResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using API.Data.Models;
+
+namespace API.DTOs.Coaches
+{
+    public class GetAthletesResponseDTO
+    {
+        public bool Success { get; set; }
+        public Dictionary<string, string>? Errors { get; set; }
+        public Dictionary<int, string>? Result { get; set; }
+    }
+}
