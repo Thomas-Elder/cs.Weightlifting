b50f435d5a10fbd58da8e28f9d841ca4f476c6a6
Thomas <thombsaway@gmail.com>
2022-03-07 13:40:16 +1100

    Added AddCoachResponseDTO

---
+++ Weightlifting/API/DTOs/Athletes/AddCoachResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs.Athletes
+{
+    public class AddCoachResponseDTO
+    {
+        public bool Success { get; set; }
+        public Dictionary<string, string>? Errors { get; set; }
+    }
+}
