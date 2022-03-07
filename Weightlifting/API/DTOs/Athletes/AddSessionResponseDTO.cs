08de538aa337939629adcc92f098307a9e815ab5
Thomas <thombsaway@gmail.com>
2022-03-07 13:35:49 +1100

    AddSessionResponseDTO

---
+++ Weightlifting/API/DTOs/Athletes/AddSessionResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs.Athletes
+{
+    public class AddSessionResponseDTO
+    {
+        public bool Success { get; set; }
+        public Dictionary<string, string>? Errors { get; set; }
+    }
+}
