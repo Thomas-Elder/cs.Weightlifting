cf3e3328e3561fe645a5d2ddcde782f872f14c82
Thomas <thombsaway@gmail.com>
2022-03-22 11:26:52 +1100

    Add sessions controller and first endpoint.

---
+++ Weightlifting/API/Controllers/SessionsController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data.Managers.Interfaces;
+using API.DTOs.Sessions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class SessionsController : Controller
+    {
+        private readonly ISessionsManager _sessionsManager;
+
+        public SessionsController(ISessionsManager sessionsManager)
+        {
+            _sessionsManager = sessionsManager;
+        }
+
+        [HttpPost("add")]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        public async Task<IActionResult> Add(AddSessionDTO addSessionDTO)
+        {
+            var result = await _sessionsManager.AddSession(addSessionDTO);
+
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+    }
+}
