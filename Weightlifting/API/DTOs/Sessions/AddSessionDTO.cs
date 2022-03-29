f25742f64a952139ff7aa8734e98ce0cf3945e82
Thomas <thombsaway@gmail.com>
2022-03-29 16:18:33 +1100

    Update session detail dtos to include exercises and sets

--- Weightlifting/API/DTOs/Sessions/AddSessionDTO.cs
+++ Weightlifting/API/DTOs/Sessions/AddSessionDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using API.DTOs.Exercises;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs.Sessions
 {
@@ -9,6 +10,8 @@ namespace API.DTOs.Sessions
 
         [Required]
         public DateTime Date { get; set; }
+
+        public IEnumerable<ExerciseDTO>? Exercises { get; set; }
     }
 
     public class AddSessionResponseDTO : ResponseDTO
--- Weightlifting/API/DTOs/Sessions/SessionDetailsDTO.cs
+++ Weightlifting/API/DTOs/Sessions/SessionDetailsDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace API.DTOs.Sessions
+﻿using API.DTOs.Exercises;
+
+namespace API.DTOs.Sessions
 {
     public class SessionDetailsDTO : ResponseDTO
     {
         public int SessionId { get; set; }
         public DateTime Date { get; set; }
+
+        public IEnumerable<ExerciseDTO>? Exercises { get; set; }
     }
 }
