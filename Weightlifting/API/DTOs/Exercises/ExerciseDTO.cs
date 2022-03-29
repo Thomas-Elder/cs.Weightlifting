e7dafdfbbbd6a2242d7fecd0cda0674f2ddbbb74
Thomas <thombsaway@gmail.com>
2022-03-29 16:18:21 +1100

    Added dtos for exercises and sets.

---
+++ Weightlifting/API/DTOs/Exercises/ExerciseDTO.cs
@@ -0,0 +1,10 @@
+﻿using API.DTOs.Sets;
+
+namespace API.DTOs.Exercises
+{
+    public class ExerciseDTO
+    {
+        public string? Name { get; set; }
+        public IEnumerable<SetDTO>? Sets { get; set; }
+    }
+}
---
+++ Weightlifting/API/DTOs/Sets/SetDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTOs.Sets
+{
+    public class SetDTO
+    {
+        public int Weight { get; set; }
+        public int SuccessfulRepetitions { get; set; }
+        public int FailedRepetitions { get; set; }
+    }
+}
