96dd76e985a3be50c27dbfa3cb9e6b69ce3d016e
Thomas <thombsaway@gmail.com>
2022-05-03 22:58:01 +1000

    Rm unnecessary line breaks.

--- Weightlifting/API/Data/Managers/Interfaces/IAthletesManager.cs
+++ Weightlifting/API/Data/Managers/Interfaces/IAthletesManager.cs
@@ -5,9 +5,7 @@ namespace API.Data.Managers.Interfaces
     public interface IAthletesManager
     {
         public bool GetAthleteId(string applicationUserId, out int athleteId);
-
         public Task<AddCoachResponseDTO> AddCoach(int athleteId, int coachId);
-
         public Task<AthleteDetailsDTO> Details(int athleteId);
         public Task<AthleteDetailsDTO> EditDetails(int athleteId, EditDetailsDTO editDetailsDTO);
     }
