7ebb334c08b0387c166b0f3c1506e9e94cf0b557
Thomas <thombsaway@gmail.com>
2023-02-03 11:53:00 +1100

    Added documentation to the SessionsManager and interface.

--- Weightlifting/API/Data/Managers/SessionsManager.cs
+++ Weightlifting/API/Data/Managers/SessionsManager.cs
@@ -8,6 +8,9 @@ using API.DTOs.Sets;
 
 namespace API.Data.Managers
 {
+    /// <summary>
+    /// A class for managing creation and updating of Sessions.
+    /// </summary>
     public class SessionsManager : ISessionsManager
     {
         private readonly WeightliftingContext _weightliftingContext;
@@ -16,6 +19,17 @@ namespace API.Data.Managers
             _weightliftingContext = weightliftingContext;
         }
 
+        /// <summary>
+        /// Adds a Session for an Athlete
+        /// </summary>
+        /// Checks if the athleteId exists in the database, and returns an AddSessionResponseDTO with Success
+        /// set to false if there is no matching athlete, and Error message in the Errors dict.  
+        /// If there is, it creates a Session, and adds all the Exercises, Sets and this Session to the database,
+        /// before returning an AddSessionResponseDTO with Success set to true.
+        /// <param name="addSessionDTO"></param>
+        /// <returns>
+        /// An AddSessionResponseDTO with the result of the action.
+        /// </returns>
         public async Task<AddSessionResponseDTO> AddSession(AddSessionDTO addSessionDTO)
         {
             var athlete = await _weightliftingContext.Athletes.FirstOrDefaultAsync(a => a.Id == addSessionDTO.AthleteId);
@@ -73,6 +87,16 @@ namespace API.Data.Managers
             };
         }
 
+        /// <summary>
+        /// Gets the details for a given sessionId.
+        /// </summary>
+        /// Checks if the sessionId matches an existing Session in the database, if not, returns a SessionDetailsDTO
+        /// with Success set to false, and Error message in the Errors dict.
+        /// If it exists, the details of the Session are returned in a SessionDetailsDTO with Success set to true.
+        /// <param name="sessionId"></param>
+        /// <returns>
+        /// A SessionDetailsDTO with the result of the action.
+        /// </returns>
         public async Task<SessionDetailsDTO> Details(int sessionId)
         {
             var session = await _weightliftingContext.Sessions.FirstOrDefaultAsync(s => s.Id == sessionId);
@@ -125,6 +149,17 @@ namespace API.Data.Managers
             };
         }
 
+        /// <summary>
+        /// Updates the details of a Session.
+        /// </summary>
+        /// Checks if the sessionId matches an existing Session in the database, if not, returns a SessionDetailsDTO
+        /// with Success set to false, and Error message in the Errors dict.
+        /// If the Session exists, the details of the Session are updated, and the updated details are returned in a 
+        /// editSessionDetailsDTO, with Success set to true.
+        /// <param name="editSessionDetailsDTO"></param>
+        /// <returns>
+        /// A editSessionDetailsDTO with the result of the action. 
+        /// </returns>
         public async Task<EditSessionDetailsResponseDTO> EditDetails(EditSessionDetailsDTO editSessionDetailsDTO)
         {
             var session = await _weightliftingContext.Sessions.FirstOrDefaultAsync(s => s.Id == editSessionDetailsDTO.SessionId);
