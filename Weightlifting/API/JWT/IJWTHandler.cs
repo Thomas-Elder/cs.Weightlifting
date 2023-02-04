ec8c920cdf3e452b5ab2af44bb1e57d2632bbd97
Thomas <thombsaway@gmail.com>
2023-02-04 11:11:35 +1100

    Added documentation for IJWTHandler and updated JWTHandler.

--- Weightlifting/API/JWT/IJWTHandler.cs
+++ Weightlifting/API/JWT/IJWTHandler.cs
@@ -7,9 +7,43 @@ namespace API.JWT
 {
     public interface IJWTHandler
     {
+        /// <summary>
+        /// Returns a string, a serialized JWTSecurityToken (JSON Web Signature in 'Compact Serialization Format'), used
+        /// by the consumer of the API to authorise their access to specified endpoints. 
+        /// </summary>
+        /// Takes an ApplicationUser, creates SigningCredentials, gets the user's claims, generates the token options, 
+        /// then creates a new JWT for the user to include in requests to the API for authorisation. 
+        /// <param name="user"></param>
+        /// <returns>
+        /// A JSON Web Token which includes serialised information about the user's permissions.
+        /// </returns>
         public Task<string> GetToken(ApplicationUser user);
+
+        /// <summary>
+        /// Generates the SigningCredentials, using the secret key stored in appsettings.json
+        /// </summary>
+        /// <returns>
+        /// Returns a new SigningCredentials
+        /// </returns>
         public SigningCredentials GetSigningCredentials();
+
+        /// <summary>
+        /// Asynchronously gets a list of Claims for this user from the UserManager.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>
+        /// Returns a task, which resolves to a list of Claims.
+        /// </returns>
         public Task<List<Claim>> GetClaims(ApplicationUser user);
+
+        /// <summary>
+        /// Creates the JWTSecurity token using the signingCredentials and claims passed. 
+        /// </summary>
+        /// <param name="signingCredentials"></param>
+        /// <param name="claims"></param>
+        /// <returns>
+        /// Returns a JwtSecurityToken.
+        /// </returns>
         public JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims);
     }
 }
--- Weightlifting/API/JWT/JWTHandler.cs
+++ Weightlifting/API/JWT/JWTHandler.cs
@@ -9,6 +9,15 @@ using API.Data.Models;
 
 namespace API.JWT
 {
+    /// <summary>
+    /// Manages creation of a JSON Web Token for authorisation.
+    /// </summary>
+    /// This process has been broken into a few steps. 
+    /// 1 - Create SigningCredentials based on our secret in appsettings.json
+    /// 2 - Get user claims from the userManager, this is where the user role is registered, so this allows
+    ///     Coaches access to Coach endpoints for example. 
+    /// 3 - Generate a JWTSecurityToken using the SigningCredentials and user Claims
+    /// 4 - This is then serialised for returning to the consumer of the API.
     public class JWTHandler : IJWTHandler
     {
         private readonly IConfigurationSection _jwtSettings;
@@ -24,15 +33,12 @@ namespace API.JWT
         /// Returns a string, a serialized JWTSecurityToken (JSON Web Signature in 'Compact Serialization Format'), used
         /// by the consumer of the API to authorise their access to specified endpoints. 
         /// </summary>
-        /// This process has been broken into a few steps. 
-        /// 1 - Create SigningCredentials based on our secret in appsettings.json
-        /// 2 - Get user claims from the userManager, this is where the user role is registered, so this allows
-        ///     Coaches access to Coach endpoints for example. 
-        /// 3 - Generate a JWTSecurityToken using the SigningCredentials and user Claims
-        /// 4 - This is then serialised for returning to the consumer of the API.
-        /// 
+        /// Takes an ApplicationUser, creates SigningCredentials, gets the user's claims, generates the token options, 
+        /// then creates a new JWT for the user to include in requests to the API for authorisation. 
         /// <param name="user"></param>
-        /// <returns></returns>
+        /// <returns>
+        /// A JSON Web Token which includes serialised information about the user's permissions.
+        /// </returns>
         public async Task<string> GetToken(ApplicationUser user)
         {
             var signingCredentials = GetSigningCredentials();
