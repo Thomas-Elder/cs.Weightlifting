a66055b42af1e51c1edbb3b9767b5276d827f2dd
Thomas <thombsaway@gmail.com>
2023-02-19 19:45:49 +1100

    Update program commenting out AccountService registration for now.

--- Weightlifting/WEB/Program.cs
+++ Weightlifting/WEB/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+/*
 builder.Services.AddScoped<IAccountService, AccountService>();
 
 builder.Services.AddHttpClient<IAccountService, AccountService>(client =>
@@ -17,7 +18,7 @@ builder.Services.AddHttpClient<IAccountService, AccountService>(client =>
     // Should be able to access app.settings here with something like:
     // config.GetRequiredSection("API_URL").Value
     client.BaseAddress = new Uri("https://localhost:7207/");
-});
+});*/
 
 builder.Services.AddHttpClient<IHomeService, HomeService>(client =>
 {
