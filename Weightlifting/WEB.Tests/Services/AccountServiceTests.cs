d619d809f1c33855ddbd46aedc37344c4c77c8e9
Thomas <thombsaway@gmail.com>
2023-02-22 11:17:47 +1100

    Implement tests for AccountService with bad and ok request responses.

--- Weightlifting/WEB.Tests/Services/AccountServiceTests.cs
+++ Weightlifting/WEB.Tests/Services/AccountServiceTests.cs
@@ -1,52 +1,71 @@
 ﻿using Xunit;
+
 using NSubstitute;
 using NSubstitute.ReturnsExtensions;
+
+using System.Net;
+using System.Threading;
+using System;
 using System.Net.Http;
+
 using WEB.Services;
-using System.Net.Http.Json;
 using WEB.ViewModels.Athletes;
-using System.Threading.Tasks;
+using WEB.Tests.Utility;
+
 
 namespace WEB.Tests.Services
 { 
     public class AccountServiceTests
     {
-        private readonly HttpClient _httpClient;
-        private readonly HttpMessageHandler mock_handler;
-        private readonly AccountService _sut;
+        private HttpClient _httpClient;
+        private AccountService _sut;
 
         public AccountServiceTests()
         {
-            mock_handler = Substitute.For<HttpMessageHandler>();
-            _httpClient = new HttpClient(mock_handler);
-
-            _sut = new AccountService(_httpClient);
+            
         }
 
-        //[Fact]
+        [Fact]
         public async void RegisterAthlete_WhenPassedInvalidRegistrationDetails_ReturnsFailureMessage()
         {
             // Arrange
             string expected = "Registration failed";
 
-            RegisterAthlete input = new RegisterAthlete()
-            {
-                FirstName = "Fail",
-                Email = "Bad email",
-                Password = "Bad password"
-            };
+            var mockHttpMessageHandler = Substitute.ForPartsOf<MockHttpMessageHandler>();
+            var httpClient = new HttpClient(mockHttpMessageHandler) { BaseAddress = new Uri("https://localhost:7207/") };
 
-            HttpResponseMessage response = new HttpResponseMessage()
-            {
-                Content = new StringContent("test content"),
-                StatusCode = System.Net.HttpStatusCode.BadRequest
-            };
+            var mockResponse = new HttpResponseMessage(HttpStatusCode.BadRequest);
 
-            _httpClient.PostAsJsonAsync("Api url", Substitute.For<RegisterAthlete>())
-                .Returns(Task.FromResult(response));
+            mockHttpMessageHandler.MockSend(Arg.Any<HttpRequestMessage>(), Arg.Any<CancellationToken>())
+               .Returns(mockResponse);
+
+            _sut = new AccountService(httpClient);
 
             // Act
-            var actual = await _sut.RegisterAthlete(input);
+            var actual = await _sut.RegisterAthlete(Substitute.For<RegisterAthlete>());
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async void RegisterAthlete_WhenPassedValidRegistrationDetails_ReturnsSuccessMessage()
+        {
+            // Arrange
+            string expected = "Registration success!";
+
+            var mockHttpMessageHandler = Substitute.ForPartsOf<MockHttpMessageHandler>();
+            var httpClient = new HttpClient(mockHttpMessageHandler) { BaseAddress = new Uri("https://localhost:7207/") };
+
+            var mockResponse = new HttpResponseMessage(HttpStatusCode.OK);
+
+            mockHttpMessageHandler.MockSend(Arg.Any<HttpRequestMessage>(), Arg.Any<CancellationToken>())
+               .Returns(mockResponse);
+
+            _sut = new AccountService(httpClient);
+
+            // Act
+            var actual = await _sut.RegisterAthlete(Substitute.For<RegisterAthlete>());
 
             // Assert
             Assert.Equal(expected, actual);
