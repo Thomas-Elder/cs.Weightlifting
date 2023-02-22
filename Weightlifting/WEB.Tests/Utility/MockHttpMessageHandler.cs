c18fe0e8ee4001804eea6b7e4db96d6427f757ad
Thomas <thombsaway@gmail.com>
2023-02-22 11:17:21 +1100

    Adding a MockHttpMessageHandler, extending the HttpMessageHandler so we can mock it's behaviour in the HttpClient.

---
+++ Weightlifting/WEB.Tests/Utility/MockHttpMessageHandler.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WEB.Tests.Utility
+{
+    public class MockHttpMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(MockSend(request, cancellationToken));
+        }
+
+        protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return MockSend(request, cancellationToken);
+        }
+
+        public virtual HttpResponseMessage MockSend(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
