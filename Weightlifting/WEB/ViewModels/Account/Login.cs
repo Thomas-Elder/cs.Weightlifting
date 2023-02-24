69fcd82d0c987429563e4871cf48e00bbdf28b39
Thomas <thombsaway@gmail.com>
2023-02-24 11:23:20 +1100

    Reorganising pages and models under Account to line up with API.

---
+++ Weightlifting/WEB/ViewModels/Account/Login.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace WEB.ViewModels.Account
+{
+    public class Login
+    {
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+    }
+}
