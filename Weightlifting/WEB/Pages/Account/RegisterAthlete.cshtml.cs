c7772fd1e45922e70594cd98d8a3e43e30bc7e2e
Thomas <thombsaway@gmail.com>
2023-05-23 13:39:48 +1000

    Added confirmpassword field, and updated cshtml

--- Weightlifting/WEB/Pages/Account/RegisterAthlete.cshtml.cs
+++ Weightlifting/WEB/Pages/Account/RegisterAthlete.cshtml.cs
@@ -1,18 +1,19 @@
+using DTO.Account;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 using WEB.Services;
-using WEB.ViewModels.Account;
 
 namespace WEB.Pages.Account
 {
     public class RegisterAthleteModel : PageModel
     {
         [BindProperty]
-        public RegisterAthlete Register { get; set; } = default!;
+        public UserRegistrationDTO Register { get; set; } = default!;
 
         private readonly IAccountService _accountService;
 
-        public string Result = default!;
+        public UserRegistrationResponseDTO Result = default!;
 
         public RegisterAthleteModel(IAccountService accountService)
         {
@@ -26,16 +27,21 @@ namespace WEB.Pages.Account
         public async Task<IActionResult> OnPostAsync()
         {
 
-            if (!ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var result = await _accountService.RegisterAthlete(Register);
 
                 Result = result;
 
                 return Page();
-            }
+            } else
+            {
+                var errors = ModelState.Select(x => x.Value.Errors)
+                           .Where(y => y.Count > 0)
+                           .ToList();
 
-            return RedirectToPage("./Index");
+                return Page();
+            }
         }
     }
 }
