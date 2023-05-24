b6147d7a263c909c8845b93f4393c4947077086c
Thomas <thombsaway@gmail.com>
2023-05-24 16:18:32 +1000

    Update the ManagerTests to ref the DTO project.

--- Weightlifting/API.Tests/Data/Managers/CoachesManagerTests.cs
+++ Weightlifting/API.Tests/Data/Managers/CoachesManagerTests.cs
@@ -5,8 +5,8 @@ using Xunit;
 using API.Data;
 using API.Data.Models;
 using API.Data.Managers;
-using API.Data.Managers.Interfaces;
-using API.DTOs.Coaches;
+
+using DTO.Coaches;
 
 namespace API.Tests.Data.Managers
 {
