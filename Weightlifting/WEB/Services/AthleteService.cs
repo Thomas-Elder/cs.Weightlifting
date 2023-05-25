f2bfecf7cab54691d5837ed7dee55aea485709fc
Thomas <thombsaway@gmail.com>
2023-05-25 11:26:02 +1000

    Add Athlete service and interface.

---
+++ Weightlifting/WEB/Services/AthleteService.cs
@@ -0,0 +1,33 @@
+﻿using DTO.Athletes;
+using WEB.Services.Interfaces;
+
+namespace WEB.Services
+{
+    public class AthleteService : IAthleteService
+    {
+        public Task<AddCoachResponseDTO> AddCoach()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> CheckAthlete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AthleteDetailsDTO> Details()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AthleteDetailsDTO> EditDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AthleteDetailsDTO> MyDetails()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Weightlifting/WEB/Services/Interfaces/IAthleteService.cs
@@ -0,0 +1,14 @@
+﻿using DTO.Athletes;
+
+namespace WEB.Services.Interfaces
+{
+    public interface IAthleteService
+    {
+        public Task<string> CheckAthlete();
+        public Task<AddCoachResponseDTO> AddCoach();
+        public Task<AthleteDetailsDTO> Details();
+        public Task<AthleteDetailsDTO> MyDetails();
+        public Task<AthleteDetailsDTO> EditDetails();
+
+    }
+}
