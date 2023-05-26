4f75d3a4000b0379540bd7813a6bc7ddfe549171
Thomas <thombsaway@gmail.com>
2023-05-26 17:05:25 +1000

    Coped services over to the Blazor app.

---
+++ Weightlifting/WEB.Blazor/Services/HomeService.cs
@@ -0,0 +1,27 @@
+﻿using WEB.Blazor.Services.Interfaces;
+
+namespace WEB.Blazor.Services
+{
+    public class HomeService : IHomeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public HomeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+
+        }
+
+        public async Task<string> CheckConnection()
+        {
+            var result = await _httpClient.GetAsync("api/home/check");
+
+            if (!result.IsSuccessStatusCode)
+            {
+                return "Connection failure";
+            }
+
+            return await result.Content.ReadAsStringAsync();
+        }
+    }
+}
