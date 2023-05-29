874fdfb103d6c6c5f28c5649d719334d2fc5fafc
Thomas <thombsaway@gmail.com>
2023-05-29 17:12:12 +1000

    Stubbing out CoachService.

---
+++ Weightlifting/WEB.Blazor/Services/CoachService.cs
@@ -0,0 +1,34 @@
+﻿using DTO.Athletes;
+using DTO.Coaches;
+using WEB.Blazor.Services.Interfaces;
+
+namespace WEB.Blazor.Services
+{
+    public class CoachService : ICoachService
+    {     
+        public Task<string> Check()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AddAthleteToCoachResponseDTO> AddAthlete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CoachDetailsDTO> Details()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CoachDetailsDTO> EditDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CoachDetailsDTO> MyDetails()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Weightlifting/WEB.Blazor/Services/Interfaces/ICoachService.cs
@@ -0,0 +1,14 @@
+﻿using DTO.Athletes;
+using DTO.Coaches;
+
+namespace WEB.Blazor.Services.Interfaces
+{
+    public interface ICoachService
+    {
+        public Task<string> Check();
+        public Task<AddAthleteToCoachResponseDTO> AddAthlete();
+        public Task<CoachDetailsDTO> Details();
+        public Task<CoachDetailsDTO> MyDetails();
+        public Task<CoachDetailsDTO> EditDetails();
+    }
+}
