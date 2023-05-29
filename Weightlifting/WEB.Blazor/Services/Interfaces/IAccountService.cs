87b0f080c648498f60048a5c0497bcadd6a8ed1d
Thomas <thombsaway@gmail.com>
2023-05-29 17:04:03 +1000

    Added RegisterCoach to the AccountService.

--- Weightlifting/WEB.Blazor/Services/Interfaces/IAccountService.cs
+++ Weightlifting/WEB.Blazor/Services/Interfaces/IAccountService.cs
@@ -7,6 +7,7 @@ namespace WEB.Blazor.Services.Interfaces
     {
         Task<string> CheckAccount();
         Task<UserRegistrationResponseDTO> RegisterAthlete(UserRegistrationDTO register);
+        Task<UserRegistrationResponseDTO> RegisterCoach(UserRegistrationDTO register);
         Task<UserAuthenticationResponseDTO> Login(UserAuthenticationDTO login);
         Task Logout();
     }
