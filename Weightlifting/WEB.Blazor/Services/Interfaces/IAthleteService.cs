daff7aa7462fa3aff0be9b41440ed34d2693e177
Thomas <thombsaway@gmail.com>
2023-06-01 22:19:07 +1000

    IMplement add and get coach functions to the athlete service.

--- Weightlifting/WEB.Blazor/Services/Interfaces/IAthleteService.cs
+++ Weightlifting/WEB.Blazor/Services/Interfaces/IAthleteService.cs
@@ -5,7 +5,8 @@ namespace WEB.Blazor.Services.Interfaces
     public interface IAthleteService
     {
         public Task<string> Check();
-        public Task<AddCoachResponseDTO> AddCoach();
+        public Task<AddCoachResponseDTO> AddCoach(int coachId);
+        public Task<GetCoachesDTO> GetCoaches();
         public Task<AthleteDetailsDTO> Details();
         public Task<AthleteDetailsDTO> MyDetails();
         public Task<AthleteDetailsDTO> EditDetails(EditDetailsDTO editDetailsDTO);
